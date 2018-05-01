d5d3c23e6ea907e947af28e8dd1baafac69acab0
Michel Jung <michel.jung89@gmail.com>
2018-05-01 23:48:18 +0200

    Improve coding standard

--- src/main/java/com/github/micheljung/transparentwebview/TransparentWebViewPatch.java
+++ src/main/java/com/github/micheljung/transparentwebview/TransparentWebViewPatch.java
@@ -1,9 +1,5 @@
-package com.github.micheljung.transparentwebview;
-
-/**
- * @author Riccardo Balbo
- * @description This patch aims to make the WebView capable to correctly render transparent pages.
- * @license Copyright (c) 2015 Riccardo Balbo < riccardo @ forkforge . net >
+/*
+ * Copyright (c) 2015 Riccardo Balbo < riccardo @ forkforge . net >
  * <p>
  * This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held
  * liable for any damages arising from the use of this software.
@@ -20,6 +16,7 @@ package com.github.micheljung.transparentwebview;
  * <p>
  * 3. This notice may not be removed or altered from any source distribution.
  */
+package com.github.micheljung.transparentwebview;
 
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -27,101 +24,79 @@ import javassist.CtMethod;
 
 import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
+
+/**
+ * This patch aims to make the WebView capable to correctly render transparent pages.
+ *
+ * @author Riccardo Balbo
+ */
 public class TransparentWebViewPatch implements ClassFileTransformer {
-  private final ClassPool _CLASS_POOL = ClassPool.getDefault();
+
+  private final ClassPool CLASS_POOL = ClassPool.getDefault();
 
   public static void premain(String agentArguments, Instrumentation instrumentation) {
     instrumentation.addTransformer(new TransparentWebViewPatch());
   }
 
-  // This patch aims to make the WebView capable to correctly render
-  // transparent pages.
-
   @Override
-  public byte[] transform(ClassLoader loader, String class_name, Class<?> class_being_redefined, ProtectionDomain protection_domain, byte[] byte_code) throws IllegalClassFormatException {
-    if (class_name.equals("com/sun/webkit/WebPage")) {
-      System.out.println("> Patching " + class_name + " ...");
+  public byte[] transform(ClassLoader loader, String className, Class<?> class_being_redefined, ProtectionDomain protection_domain, byte[] byteCode) {
+    if (className.equals("com/sun/webkit/WebPage")) {
+      System.out.println("> Patching " + className + " ...");
       try {
-        CtClass ct_class = _CLASS_POOL.makeClass(new ByteArrayInputStream(byte_code));
+        CtClass ctClass = CLASS_POOL.makeClass(new ByteArrayInputStream(byteCode));
 
         // First thing to do is edit the fireLoadEvent in order to force
         // the page to set its own
         // background to transparent black (0x00000000 argb) each time
         // it changes
-        CtMethod fireLoadEvent_method = ct_class.getDeclaredMethod("fireLoadEvent");
-        fireLoadEvent_method.insertBefore("{\n" + "    "
+        CtMethod fireLoadEventMethod = ctClass.getDeclaredMethod("fireLoadEvent");
+        fireLoadEventMethod.insertBefore("{\n" + "    "
             + "setBackgroundColor(0);\n"
             + "}");
-
-        // DEPRECATED CODE: NOT WORKING FROM JAVA 1.8.0_66
-        // Then, we edit the the paint2GC method in order to call
-        // clearRect every time right before it
-        // start to draw a clip (clips are parts of the rendered frame).
-        // We need this because when the webpage is rendered with the
-        // backbuffer enabled,
-        // every clip is just drawn over the old rendered frame without
-        // care about the alpha channel.
-        // NOTE: there is a system property
-        // com.sun.webkit.pagebackbuffer that could do the trick without
-        // this entire patch, but i didn't succeed in getting anything
-        // useful out of it...
-        // CtMethod
-        // paint2GC_method=ct_class.getDeclaredMethod("paint2GC");
-        // paint2GC_method.insertAt(696,
-        // "{\n" +
-        // " com.sun.webkit.graphics.WCRectangle clip=$1.getClip(); \n"
-        // +
-        // "
-        // $1.clearRect(clip.getX(),clip.getY(),clip.getWidth(),clip.getHeight());\n"
-        // +
-        // "}"
-        // );
-        // END OF DEPRECATED CODE
 
         // Then we replace the scroll method body in order to force the
         // repaint of the entire frame
         // when the page is scrolled
-        CtMethod scroll_method = ct_class.getDeclaredMethod("scroll");
-        scroll_method.setBody(
+        CtMethod scrollMethod = ctClass.getDeclaredMethod("scroll");
+        scrollMethod.setBody(
             "{\n" + "   "
                 + "addDirtyRect(new com.sun.webkit.graphics.WCRectangle(0f,0f,(float)width,(float)height));\n"
                 + "}"
         );
-        byte_code = ct_class.toBytecode();
-        ct_class.detach();
+        byteCode = ctClass.toBytecode();
+        ctClass.detach();
       } catch (Exception e) {
-        System.out.println("/!\\ " + class_name + " patching failed :(");
+        System.out.println("/!\\ " + className + " patching failed :(");
         e.printStackTrace();
-        return byte_code;
+        return byteCode;
       }
-      System.out.println("> " + class_name + " patching succeeded!");
-    } else if (class_name.equals("com/sun/javafx/webkit/prism/WCGraphicsPrismContext")) {
-      System.out.println("> Patching " + class_name + " ...");
+      System.out.println("> " + className + " patching succeeded!");
+    } else if ("com/sun/javafx/webkit/prism/WCGraphicsPrismContext".equals(className)) {
+      System.out.println("> Patching " + className + " ...");
       try {
-        CtClass ct_class = _CLASS_POOL.makeClass(new ByteArrayInputStream(byte_code));
+        CtClass ctClass = CLASS_POOL.makeClass(new ByteArrayInputStream(byteCode));
 
         // Then, we edit the the WCGraphicsPrismContext.setClip method
         // in order to call clearRect over the area of the clip.
-        CtClass signature[] = new CtClass[]{_CLASS_POOL.get("com.sun.webkit.graphics.WCRectangle")};
-        CtMethod setClip_method = ct_class.getDeclaredMethod("setClip", signature);
-        setClip_method.insertBefore(
+        CtClass signature[] = new CtClass[]{CLASS_POOL.get("com.sun.webkit.graphics.WCRectangle")};
+        CtMethod setClipMethod = ctClass.getDeclaredMethod("setClip", signature);
+        setClipMethod.insertBefore(
             "{" + "  "
                 + " $0.clearRect($1.getX(),$1.getY(),$1.getWidth(),$1.getHeight());"
                 + "}");
-        byte_code = ct_class.toBytecode();
-        ct_class.detach();
+        byteCode = ctClass.toBytecode();
+        ctClass.detach();
       } catch (Exception e) {
-        System.out.println("/!\\ " + class_name + " patching failed :(");
+        System.out.println("/!\\ " + className + " patching failed :(");
         e.printStackTrace();
-        return byte_code;
+        return byteCode;
       }
-      System.out.println("> " + class_name + " patching succeeded!");
+      System.out.println("> " + className + " patching succeeded!");
     }
 
-    return byte_code;
+    return byteCode;
   }
 }
